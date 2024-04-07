ffb236985fb271fc084644660e66768d51be6fd6
MehdiChebbi13 <mehdi.chebbi.fst@gmail.com>
2024-04-07 05:34:18 +0100

    commit
    commit jdid

--- server/routes/task.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createTask, getTasks } from "../controllers/tasks.js";
-
-const router = express.Router();
-
-router.post("/createtask", createTask);
-router.get("/gettask", getTasks);
-
-export default router;
